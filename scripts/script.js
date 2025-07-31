22ef5309de11c6296a0a48b2ee85e76255f4e4d5
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-07-31 17:51:38 +0800

    JS can now read form data upon submission

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,37 @@
-const dialog = document.querySelector("dialog");
 const addBookButton = document.querySelector("#add-book");
+const dialog = document.querySelector("dialog");
+const newBookForm = document.querySelector("form");
+const confirmBookButton = document.querySelector("#confirm-book");
 
 const myLibrary = [];
 
 // Events
-addBookButton.addEventListener("click", () => {
+addBookButton.addEventListener("click", (e) => {
   dialog.showModal();
+});
+
+confirmBookButton.addEventListener("click", (e) => {
+  if (!newBookForm.reportValidity()) {
+    return;
+  }
+
+  const bookForm = new FormData(newBookForm);
+
+  addToLibrary(
+    bookForm.get("title"),
+    bookForm.get("author"),
+    bookForm.get("page-count"),
+    bookForm.get("read-status"),
+  );
+
+  dialog.close();
+  newBookForm.reset();
+
+  e.preventDefault();
+
+  for (let item of myLibrary) {
+    console.log(item);
+  }
 });
 
 function Book(title, author, pageCount, read) {
